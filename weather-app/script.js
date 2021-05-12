ae1a1927a73eacd62a64304c1641cfdf559057e4
pieroborrelli-dev <80548300+pieroborrelli-dev@users.noreply.github.com>
2021-05-12 21:52:39 +0200

    Create script.js

---
+++ weather-app/script.js
@@ -0,0 +1,40 @@
+'use strict'
+const locationInfo = document.querySelector('#location')
+const locationDescription = document.querySelector('.desc')
+const celsiusTemperatureInfo = document.querySelector('.celsius')
+const fahrenheitTemperatureInfo = document.querySelector('.fahrenheit')
+const iconImg = document.getElementById('weather-icon')
+
+const apiKey = '68aa7d88c059eeaa568b6e775820f50d'
+window.addEventListener('load', () => {
+  if (navigator.geolocation) {
+    // if the user has geolocation active
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+      fetch(base)
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          const location = data.name
+          const description = data.weather[0].description
+          const celsiusTemperature = data.main.temp
+          const icon = data.weather[0].icon
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+          locationInfo.textContent = location
+          locationDescription.textContent = description
+          celsiusTemperatureInfo.textContent = `${celsiusTemperature.toFixed(
+            2
+          )}°C`
+
+          const fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32
+          fahrenheitTemperatureInfo.textContent = `${fahrenheitTemperature.toFixed(
+            2
+          )}°F`
+          iconImg.src = iconUrl // async
+        })
+    })
+  }
+})
